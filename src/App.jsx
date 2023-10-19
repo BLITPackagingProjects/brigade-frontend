a55a722dcb36c4cf0c52ee0518f9f62855ef99d1
Iovanni Garcia Hernandez <iogarher@outlook.com>
2023-10-19 13:11:37 -0400

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,14 @@
-<<<<<<< HEAD
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import UploadPetImage from './components/UploadPetImage'
 import RegisterPet from './components/PetRegistration'
 import DisplayImage from './components/DisplayImage'
-=======
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import './App.css';
-import AllPetList from './components/AllPetList';
-import Login from "./components/Login";
-import Navbar from './components/Navbar';
-import NotFound from './components/NotFound';
->>>>>>> origin/HusnaFrontBranch
 
 function App() {
 
   return (
     <div>
-<<<<<<< HEAD
-    {/* <UploadPetImage/> */}
-    {/* <RegisterPet/> */}
-    <DisplayImage/>
-=======
-      
+  
       <Router >
       <Navbar />
         <Routes>
@@ -36,7 +19,6 @@ function App() {
         </Routes>
         
     </Router>
->>>>>>> origin/HusnaFrontBranch
     </div>
   )
 }
