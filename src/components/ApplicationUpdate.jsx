d9c221150870d88c64fd0c3a01dbb23c59cbe512
Shyam Sharma <shyamsharma.sharma797@gmail.com>
2023-10-24 11:42:12 -0400

    Commit

---
+++ src/components/ApplicationUpdate.jsx
@@ -0,0 +1,30 @@
+import React, { useRef } from 'react'
+import axios from 'axios';
+
+const ApplicationUpdate = (props) => {
+
+  const statusRef =  useRef();
+
+const handleSubmit = (e)=>{
+    e.preventDefault();
+    // console.log(statusRef.current.value)
+    const obj = {
+        status:statusRef.current.value
+      }
+      axios.put(`http://localhost:9090/application/${props.match.params.id}`,obj).then((res)=>console.log(res.data))
+      props.history.replace("/application")
+}
+  return (
+    <div>
+    <form action="PUT" onSubmit={handleSubmit}>
+        <label htmlFor="status">Status</label>
+        <input id="status" type="text" ref={statusRef}/>
+
+        <button className='btn btn-success'>Update</button>
+
+    </form>
+    </div>
+  )
+}
+
+export default ApplicationUpdate
